eebd04cf3f7937710fb44dd7dfe2a52800c66877
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-04 23:07:06 -0500

    Update urls.py
    added imports for records and path

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import welcome
+from .views import welcome, records
 from .views import RecipeListView, RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', welcome, name='home'),
+    path('recipes/', records, name='records'),
     path('list/', RecipeListView.as_view(), name='list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]

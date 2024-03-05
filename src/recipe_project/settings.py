a4e5d61b12193e3ae8a2eac7a00c2f3addaf0fe3
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-04 23:05:56 -0500

    Update settings.py
    Added BASE_DIR

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'recipe_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR  / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,7 @@ USE_TZ = True
 
 #AUTH
 LOGIN_URL='/login/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/

aadefab0e6ae2baebf18cae358cbd5d8ba5c0a1d
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-04 23:07:28 -0500

    Update views.py
    added login requirement for detail and list view

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   #to display lists
 from .models import Recipe                #to access Recipes model
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
+#to protect function-based views
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -10,10 +14,16 @@ def welcome(request):
     return render(request, 'recipes/recipes_home.html')
 
 # Create your views here.
-class RecipeListView(ListView):           #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):           #class-based view
     model = Recipe                         #specify model
     template_name = 'recipes/main.html'    #specify template 
 
-class RecipeDetailView(DetailView):                       #class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):                       #class-based view
     model = Recipe                                        #specify model
     template_name = 'recipes/detail.html'                 #specify template
+
+#define function-based view - records(records()
+@login_required
+def records(request):
+    #do nothing, simply display page    
+    return render(request, 'recipes/records.html')

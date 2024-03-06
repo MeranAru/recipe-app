1bccf93e189391051ce4bf0366485b791472767d
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-06 10:26:40 -0500

    Update urls.py
    updated paths

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import welcome, records
-from .views import RecipeListView, RecipeDetailView
+from .views import home, RecipeListView, RecipeDetailView, records
+from django.views.generic import RedirectView
 
 app_name = 'recipes'
 
+
 urlpatterns = [
-    path('', welcome, name='home'),
-    path('recipes/', records, name='records'),
-    path('list/', RecipeListView.as_view(), name='list'),
-    path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path("", RedirectView.as_view(url="home/")),
+    path('home/', home, name='home'),
+    path('recipes/list/', RecipeListView.as_view(), name='list'),
+    path('recipes/list/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path('recipes/search', records, name='records')
 ]

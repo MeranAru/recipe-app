f46a11e73051dba58228114fd913ed3a22bed3f6
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-06 10:20:56 -0500

    Update forms.py
    Added difficulty options

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,14 +1,23 @@
-from django import forms
+from django import forms  # import django forms
 
-CHART__CHOICES = (          #specify choices as a tuple
-    ('#1', 'Bar chart'),    # when user selects "Bar chart", it is stored as "#1"
+CHART__CHOICES = (  # specify choices as a tuple
+    # when user selects "Bar chart", it is stored as "#1"
+    ('#1', 'Bar chart'),
     ('#2', 'Pie chart'),
     ('#3', 'Line chart')
 )
-#define class-based Form imported from Django forms
-class RecipesSearchForm(forms.Form): 
-    recipe_title= forms.CharField(max_length=120)
-    chart_type = forms.ChoiceField(choices=CHART__CHOICES)
 
-    # Add recipes_df as a hidden field
-    recipes_df = forms.CharField(widget=forms.HiddenInput(), required=False)
+DIFFIC__CHOICES = (  # specify choices as a tuple
+    # when user selects "Easy", it is stored as "#1"
+    ('#1', 'Easy'),
+    ('#2', 'Medium'),
+    ('#3', 'Intermediate'),
+    ('#4', 'Hard')
+)
+
+# define class-based Form imported from Django forms
+
+
+class RecipesSearchForm(forms.Form):
+    recipe_diff = forms.ChoiceField(choices=DIFFIC__CHOICES)
+    chart_type = forms.ChoiceField(choices=CHART__CHOICES)

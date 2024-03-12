6f38f80ecd3018b692433704f8d64ad8e1306867
Meran Arumugam <meran.arumugam@gmail.com>
2024-03-12 15:28:57 -0400

    Update settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -122,7 +122,7 @@ LOGIN_URL='/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'recipe/static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
